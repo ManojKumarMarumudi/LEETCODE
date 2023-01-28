0dc22407befb8c9571443601a2b2f47fd811b4a9
ManojKumarMarumudi <84531888+ManojKumarMarumudi@users.noreply.github.com>
2023-01-28 17:02:29 +0530

    Create amsrong_number.java

---
+++ 28-01-2023/amsrong_number.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Amstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int originalNum = num;
+        int result = 0;
+        int n = 0;
+        while (originalNum != 0) {
+            originalNum /= 10;
+            n++;
+        }
+        System.out.println(n);
+        originalNum = num;
+        while (originalNum != 0) {
+            int remainder = originalNum % 10;
+            result += Math.pow(remainder, n);
+            originalNum /= 10;
+        }
+        if (result == num) {
+            System.out.println(num + " is an Armstrong number.");
+        } else {
+            System.out.println(num + " is not an Armstrong number.");
+        }
+    }
+}
